9b68ae23860a30a951ec7300f3e9a0612e7586d6
sik1j <karkisikij2@gmail.com>
2022-05-08 10:29:38 -0700

    Add optional prop className to Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,13 +8,14 @@ interface Props {
   className?: string;
 }
 
-const Button: React.FC<Props> = ({ children, href, filled }) => {
+const Button: React.FC<Props> = ({ children, href, filled, className }) => {
   return (
     <Link href={href}>
       <a
         className={
           `${filled ? "bg-secondary text-white" : "bg-white text-secondary"}` +
-          " text-link font-semibold py-4 px-6 rounded-md border-2 border-secondary"
+          " text-link font-semibold py-4 px-6 rounded-md border-2 border-secondary " +
+          className
         }
       >
         {children}
