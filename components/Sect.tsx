0b234c1374a2473ff45668b595d6aa8f0cbae888
sik1j <karkisikij2@gmail.com>
2022-05-09 21:14:07 -0700

    Add className prop to avoid excessive wrapper divs in sections

--- components/Sect.tsx
+++ components/Sect.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Sect: React.FC<Props> = ({ children }) => {
-  return <section className="px-[5vw] py-2">{children}</section>;
+const Sect: React.FC<Props> = ({ children, className }) => {
+  return (
+    <section className={`px-[5vw] py-2 relative ${className}`}>
+      {children}
+    </section>
+  );
 };
 
 export default Sect;
