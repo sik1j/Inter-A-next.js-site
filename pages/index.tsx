ea9e9b5a86c20cb3f02c5f2ef0df0de2500aeb0e
sik1j <karkisikij2@gmail.com>
2022-05-09 21:26:50 -0700

    Add home page sections. Testimoinal left unused

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Nav from "../components/Nav";
+import Nav from "../components/layout/Nav";
+import Hero from "../components/index/Hero";
+import Operation from "../components/index/Operation";
+import Ideals from "../components/index/Ideals";
+import Difference from "../components/index/Difference";
+import Family from "../components/index/Family";
+import Teachers from "../components/index/Teachers";
+import Testimonial from "../components/index/Testimonial";
+import Footer from "../components/layout/Footer";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div>
       <Head>
         <title>Inter-A | Home</title>
       </Head>
       <Nav />
-    </>
+      <div className="flex flex-col gap-y-sect sm:bg-emerald-400">
+        <Hero />
+        <Operation />
+        <Ideals />
+        <Difference />
+        <Family />
+        <Teachers />
+        {/* <Testimonial /> */}
+        <Footer />
+      </div>
+    </div>
   );
 };
 
