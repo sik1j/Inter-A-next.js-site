773a06756aa84990df28b3922e838ab7659baf04
sik1j <karkisikij2@gmail.com>
2022-05-10 17:30:50 -0700

    Add nav styling for screen lg | Change text to Inter-A text

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -11,15 +11,17 @@ const Nav: React.FC<Props> = ({}) => {
 
   const currPath = router.pathname;
   return (
-    <div className=" flex justify-between overflow-x-hidden px-[5vw] py-2 fixed top-0 left-0 right-0 bg-white z-10">
-      <div className="text-primary text-logo-nav font-semibold">DigitUX</div>
+    <div className=" flex justify-between overflow-x-hidden px-[5vw] py-2 fixed top-0 left-0 right-0 bg-white z-10 lg:px-[10vw]">
+      <div className="text-primary text-logo-nav font-semibold  md:flex-1 md:flex md:items-center lg:text-lg-logo-nav">
+        Inter-A
+      </div>
       <div
         className={
           `${isNavOn ? " " : "-translate-y-[100vh]"}` +
-          " transition-transform fixed inset-0  flex flex-col items-center justify-between py-12 bg-white bg-opacity-70 backdrop-blur-sm "
+          " transition-transform fixed inset-0  flex flex-col items-center justify-between py-12 bg-white bg-opacity-70 backdrop-blur-sm md:translate-y-0 md:flex-row md:backdrop-blur-none md:bg-opacity-100 md:static md:flex-[2] md:py-0 "
         }
       >
-        <ul className="flex-grow-[1] flex flex-col justify-between items-center">
+        <ul className="flex-[1] flex flex-col justify-between items-center md:flex-row ">
           <li>
             <NavLink href="/" currPath={currPath}>
               Home
@@ -36,14 +38,14 @@ const Nav: React.FC<Props> = ({}) => {
             </NavLink>
           </li>
         </ul>
-        <div className="flex-grow-[1] flex flex-col justify-end">
+        <div className="flex-[1] flex flex-col justify-end md:flex-row">
           <Button href="/apply" filled plus>
             Get a Quote
           </Button>
         </div>
       </div>
       <div
-        className="flex flex-col gap-[5px] z-10 origin-right scale-75"
+        className="flex flex-col gap-[5px] z-10 origin-right scale-75 md:hidden"
         onClick={() => setIsNavOn(!isNavOn)}
       >
         <div className="w-[25px] h-[3px] bg-black" />
