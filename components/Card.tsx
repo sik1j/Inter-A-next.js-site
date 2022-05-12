32342e2fb920fa5174bdf59111ef988e61bf07f0
sik1j <karkisikij2@gmail.com>
2022-05-11 21:36:37 -0700

    Add styling for screen lg

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 const Card: React.FC<Props> = ({ title, info }) => {
   return (
-    <div className="flex flex-col justify-between space-y-3">
+    <div className="flex flex-col justify-between space-y-3 lg:justify-between lg:space-y-0">
       <div className="font-bold text-sect2-heading text-primary">{title}</div>
-      <div className="text-text-grey max-w-prose">{info}</div>
+      <div className="text-text-grey max-w-prose lg:pt-0">{info}</div>
       <Button href="/" className="w-fit" chevron>
         Learn More
       </Button>
