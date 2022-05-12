c40a51e7eb286867bdd591a48489db3e1dd68c67
sik1j <karkisikij2@gmail.com>
2022-05-11 21:44:48 -0700

    Fix mobile overflow; Add Link to logo

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -2,22 +2,26 @@ import React, { useState } from "react";
 import NavLink from "../NavLink";
 import { useRouter } from "next/router";
 import Button from "../Button";
+import Link from "next/link";
 
 interface Props {}
 
 const Nav: React.FC<Props> = ({}) => {
   const [isNavOn, setIsNavOn] = useState(false);
-  const router = useRouter();
 
+  const router = useRouter();
   const currPath = router.pathname;
+
   return (
-    <div className=" flex justify-between overflow-x-hidden px-[5vw] py-2 fixed top-0 left-0 right-0 bg-white z-10 lg:px-[10vw]">
+    <div className=" flex justify-between overflow-x-hidden  py-2 fixed top-0 left-0 right-0 bg-white z-10 container">
       <div className="text-primary text-logo-nav font-semibold  md:flex-1 md:flex md:items-center lg:text-lg-logo-nav">
-        Inter-A
+        <Link href={"/"}>
+          <a>Inter-A</a>
+        </Link>
       </div>
       <div
         className={
-          `${isNavOn ? " " : "-translate-y-[100vh]"}` +
+          `${isNavOn ? " " : "-translate-y-[110vh]"}` +
           " transition-transform fixed inset-0  flex flex-col items-center justify-between py-12 bg-white bg-opacity-70 backdrop-blur-sm md:translate-y-0 md:flex-row md:backdrop-blur-none md:bg-opacity-100 md:static md:flex-[2] md:py-0 "
         }
       >
@@ -29,12 +33,12 @@ const Nav: React.FC<Props> = ({}) => {
           </li>
           <li>
             <NavLink href="/about" currPath={currPath}>
-              About
+              Who are We?
             </NavLink>
           </li>
           <li>
-            <NavLink href="/next" currPath={currPath}>
-              Next
+            <NavLink href="/contact" currPath={currPath}>
+              Contact Us
             </NavLink>
           </li>
         </ul>
