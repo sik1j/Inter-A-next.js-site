6740cfbda86ba019b2adbef7f66ea0659388cd56
sik1j <karkisikij2@gmail.com>
2022-05-12 20:31:58 -0700

    Restructure className

--- components/SectHeading.tsx
+++ components/SectHeading.tsx
@@ -8,7 +8,7 @@ interface Props {
 const SectHeading: React.FC<Props> = ({ children, className }) => {
   return (
     <div
-      className={`${className} text-primary font-semibold text-base mb-4 lg:text-[1.375rem] lg:mb-6`}
+      className={`text-primary font-semibold text-base mb-4 lg:text-[1.375rem] lg:mb-6 ${className} `}
     >
       {children}
     </div>
