455d00fbcc9bc240a7617648f0d92337e0f9c2e2
sik1j <karkisikij2@gmail.com>
2022-05-13 22:39:59 -0700

    Extract mock data to mock/moc-card-data.tsx

--- components/index/Difference.tsx
+++ components/index/Difference.tsx
@@ -7,28 +7,11 @@ import { UserGroupIcon } from "@heroicons/react/solid";
 import { HeartIcon } from "@heroicons/react/solid";
 import { BookOpenIcon } from "@heroicons/react/solid";
 import ThinkingSVG from "../svg/ThinkingSVG";
+import { CARDS_DATA } from "../../mock/mock-card-data";
 
 interface Props {}
 
 const Difference: React.FC<Props> = ({}) => {
-  const cardsArr = [
-    {
-      title: "Participation in Community Service",
-      info: "Participation in Community Service is a key value of the Inter-A program",
-    },
-    {
-      title: "Field Trip supported Learning",
-      info: "Field studies  are much more common in Inter-A than in a traditional high school",
-    },
-    {
-      title: "Career and Personal Planning",
-      info: "A class that teaches goal setting, planning, and personal responsibility",
-    },
-    {
-      title: "Physical Education and Studio Arts",
-      info: "Students have opportunities to practice their leadership in Physical Edcuation and Arts",
-    },
-  ];
   return (
     <Sect>
       <SectHeading>What Makes Us Different</SectHeading>
@@ -43,10 +26,8 @@ const Difference: React.FC<Props> = ({}) => {
           </div>
         </div>
         <div className="space-y-11 lg:col-span-6 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-y-20 lg:pb-10 lg:gap-x-8">
-          {cardsArr.map((cardsInfo: { title: string; info: string }, ind) => {
-            return (
-              <Card key={ind} title={cardsInfo.title} info={cardsInfo.info} />
-            );
+          {CARDS_DATA.map(({ title, info }, ind) => {
+            return <Card key={ind} title={title} info={info} />;
           })}
         </div>
       </div>
---
+++ mock/mock-card-data.tsx
@@ -0,0 +1,18 @@
+export const CARDS_DATA = [
+  {
+    title: "Participation in Community Service",
+    info: "Participation in Community Service is a key value of the Inter-A program",
+  },
+  {
+    title: "Field Trip supported Learning",
+    info: "Field studies  are much more common in Inter-A than in a traditional high school",
+  },
+  {
+    title: "Career and Personal Planning",
+    info: "A class that teaches goal setting, planning, and personal responsibility",
+  },
+  {
+    title: "Physical Education and Studio Arts",
+    info: "Students have opportunities to practice their leadership in Physical Edcuation and Arts",
+  },
+];
